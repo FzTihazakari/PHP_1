407642e5edb0ad46821aa9fe647ad7aa1da0a252
Tiha Zakari <fzfatihahzakari@gmail.com>
2023-06-07 09:27:44 +0800

    Add files via upload

---
+++ ArrayCar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$cars = array("Proton","Audi","Perodua","Toyota","Honda","Mazda");
+foreach ($cars as $value) {
+
+    echo "Car type is $value <br>";
+}
+?>
+
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Lab 6 Q1</title>
+</head>
+<body>
+<?php
+$name = "Muhammad Asyraf";
+$matricno = "Ai210087";
+$course = "Bachelor In Information Technology";
+$year = "2";
+$address = "Seri Kembangan, Selangor";
+?>
+<table>
+<tr>
+    <td>Name</td>
+    <td><?php echo "$name"; ?></td>
+<tr>
+    <td>Matric No</td>
+    <td><?php echo "$matricno"; ?></td>
+</tr>
+<tr>
+    <td>Course</td>
+    <td><?php echo "$course"; ?></td>
+</tr>
+<tr>
+    <td>Year</td>
+    <td><?php echo "$year"; ?></td>
+</tr>
+<tr>
+    <td>Address</td>
+    <td><?php echo "$address"; ?></td>
+</tr>
+</table>
+</body>
+</html>
